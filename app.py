0ab5eeefe745cce77115f9c17e44ada834ad27a1
Djaymes <yunoastral@gmail.com>
2025-05-01 08:47:18 +0100

    App test number 1

---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+# Load the dataset
+movies_imdb = pd.read_csv('IMDB Top 250 Movies.csv')
+
+st.header('Best Movies of All Time')
+st.subheader('IMDB Top 250 Movies')
+
+movies_data = pd.read_csv('IMDB Top 250 Movies.csv') # lendo os dados
+hist_button = st.button('Criar histograma') # criar um botão
+        
+if hist_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de dados dos melhores filmes...')
+            
+    # criar um histograma
+    fig = px.histogram(movies_data, x = "rating")
+            
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Criar gráfico de dispersão') # criar um botão
+
+if scatter_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um gráfico de dispersão para o conjunto de dados dos melhores filmes...')
+            
+    # criar um histograma
+    fig = px.scatter(movies_data, x = "rating", y = 'genre')
+            
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width = True)
